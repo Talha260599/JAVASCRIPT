b4ce0593844f6a5e03bfe5ce61b47b793534c1b7
Talukder Abdullah Al Talha <71594697+Talha260599@users.noreply.github.com>
2023-12-17 22:06:32 +0600

    Add files via upload

---
+++ findIndex.js
@@ -0,0 +1,11 @@
+var nums,i,j;
+
+nums = [2, 7, 11, 15,98,56,7,25,56,88,99];
+j=nums.length;
+
+// Print array indices
+for (i = 0; i < j; i++) 
+{
+    document.write(i + "=" +nums[i] + "<br/>");
+
+}
