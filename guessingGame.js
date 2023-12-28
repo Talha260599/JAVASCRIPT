eca05215df9bd88576dc378e47913954258d4d10
Talukder Abdullah Al Talha <71594697+Talha260599@users.noreply.github.com>
2023-12-28 19:20:26 +0600

    Guess the number

---
+++ guessingGame.js
@@ -0,0 +1,44 @@
+var i,num,randomNum,matchNum=0,unmatchNum=0;
+
+for(i=1;i<6;i++)
+
+{
+
+num=parseInt(prompt("Enter num from 1 to 5 : "));
+
+randomNum=Math.floor(Math.random() * 6);
+
+if(num==randomNum)
+
+{
+
+        document.write("You have won <br>");
+
+        matchNum++;
+
+}
+
+else 
+
+{
+
+        document.write("You have lost <br>");
+        
+        unmatchNum++
+
+}
+
+}
+
+document.write("<br><br><br>");
+
+document.write("You total won : " +matchNum + "<br/>");
+
+document.write("You total lost : " +unmatchNum);
+
+
+
+
+
+
+
